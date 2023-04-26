17f5d247723a2422fbafc8e878ff923f02ef8b66
Saket Nanda <saketnanda164@gmail.com>
2023-04-26 18:07:39 +0530

    added changes to style and index.html script

--- static/script.js
+++ static/script.js
@@ -19,26 +19,26 @@
 
 
 
-console.log(13);
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("document loaded");
-    // var iframe = document.getElementById('my-iframe');
-    // var localServerUrl = 'http://127.0.0.1:5000'; // Replace with your local server URL
-    // iframe.src = chrome.runtime.getURL('index.html') + '?url=' + encodeURIComponent(localServerUrl);
-    // iframe.src=localServerUrl;
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        console.log("query loaded")
-        let url = tabs[0].url;
-        console.log(url);
-        // var iframe = document.getElementById('my-iframe');
+// console.log(13);
+// document.addEventListener('DOMContentLoaded', function() {
+//     console.log("document loaded");
+//     // var iframe = document.getElementById('my-iframe');
+//     // var localServerUrl = 'http://127.0.0.1:5000'; // Replace with your local server URL
+//     // iframe.src = chrome.runtime.getURL('index.html') + '?url=' + encodeURIComponent(localServerUrl);
+//     // iframe.src=localServerUrl;
+//     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+//         console.log("query loaded")
+//         let url = tabs[0].url;
+//         console.log(url);
+//         // var iframe = document.getElementById('my-iframe');
         
-        let input_url=document.getElementById('url');
-        // let btn=iframe.contentWindow.document.getElementById('btn');
-        input_url.innerHTML=url;
-        // btn.click();
-        // use `url` here inside the callback because it's asynchronous!
-    });
-  });
+//         let input_url=document.getElementById('url');
+//         // let btn=iframe.contentWindow.document.getElementById('btn');
+//         input_url.innerHTML=url;
+//         // btn.click();
+//         // use `url` here inside the callback because it's asynchronous!
+//     });
+//   });
 
 
 
